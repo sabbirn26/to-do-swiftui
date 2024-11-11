a7e7218d38c0ae1d9f7342605f20835c02cd0d38
Sabbir <nsabbir26@gmail.com>
2024-11-11 12:28:50 +0600

    init commit

---
+++ TaskNest/TaskNest/TaskNestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskNestApp.swift
+//  TaskNest
+//
+//  Created by Sabbir Nasir on 11/11/24.
+//
+
+import SwiftUI
+
+@main
+struct TaskNestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
